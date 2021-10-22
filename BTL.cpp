223c07281d97660387ac38fcfb44b37bbec0c248
Longk61 <81979929+Longk61@users.noreply.github.com>
2021-10-22 09:37:05 +0700

    Update BTL.cpp
    update

--- BTL.cpp
+++ BTL.cpp
@@ -1,264 +1,230 @@
-#include<stdio.h>
-#include<conio.h>
-#include<stdlib.h>
+#include<iostream>
 #include<string.h>
-#define max 100
-struct Employee{
-	char name[40];
-	char province[40];
-	int age;
-	int msnv;
-	char truong[40];
+#include<stdlib.h>
+#include<math.h>
+using namespace std;
+class Diem {
+	protected:
+		float Toan;
+		float Ly;
+		float Hoa;
+		float Van;
+		float Su;
+		float Dia;
+	public:
+		Diem();
+		Diem(float Toan,float Ly,float Hoa ,float Van ,float Su,float Dia);
+		~Diem();
+		void Nhap1();
+		void Xuat1();
 };
-typedef Employee NV;
-struct Danhsach{
-	NV a[max];
-	int n;// so luong nhan vien = n
+class HocSinh: public Diem{
+	protected:
+	  string  NgaySinh, Ten , Lop , DiaChi, MSHS;
+    public:
+    	HocSinh();
+    	HocSinh(string Ten, string Lop, string DiaChi, string NgaySinh, string MSHS);
+    	~HocSinh();
+	    void Nhap();
+	    void Xuat();
 };
-typedef Danhsach danhsach;
-void Nhap(NV  &x);
-void NhapN(danhsach &ds);
-void Xuat(NV x);
-void XuatN(danhsach ds);
-void ThemNV(danhsach &ds , int b);
-void TimNvTheotinh(danhsach ds , char c[] );
-void Sapxep(danhsach ds );
-void ThongKe(danhsach ds);
-int TimMSVV(danhsach ds, int d);
-void xuatFile(danhsach ds,char fileName[] );
-void menu();
-
-int main(){
-	system("color F0");
-	menu();
-	
+Diem::Diem(){
+	Toan=0;
+	Ly=0;
+	Hoa=0;
+	Van=0;
+	Su=0;
+	Dia=0;
 }
-void Nhap(NV &x){
+Diem::Diem(float Toan, float Ly, float Hoa, float Van,float Su, float Dia){
+	this->Toan=Toan;
+	this->Ly=Ly;
+	this->Hoa=Hoa;
+	this->Van=Van;
+	this->Su=Su;
+	this->Dia=Dia;
+}
+Diem::~Diem(){
+	this->Toan=0;
+	this->Ly=0;
+	this->Hoa=0;
+	this->Van=0;
+	this->Su=0;
+	this->Dia=0;
+}
+void Diem::Nhap1() {
+    fflush(stdin);
+    cout<<"\nNhap Diem Toan: ";
+	cin >> Toan;
+    cout<<"Nhap Diem Ly: " ;
+	cin >> Ly;
+    cout<<"Nhap Diem Hoa: " ;
+	cin >> Hoa;
+    cout<<"Nhap Diem Van: ";
+	cin >> Van;
+    cout<<"Nhap Diem Su: ";
+	cin >> Su;
+    cout<<"Nhap Diem Dia: ";
+	cin >>Dia;
+   
+}
+void Diem:: Xuat1(){
+    cout<<"\n  Diem Cua Hoc Sinh : "<<endl;
+    cout<<"\tToan : " <<Toan<<"  |\tVan : "<<Van<<" |\tSu : "<<Su  <<" |\tDia : " <<Dia<<"  |\tLy : "<<Ly << " |\tHoa: " <<Hoa<<endl;
+}
+HocSinh::HocSinh(){
+	Ten ="";
+	Lop ="";
+	NgaySinh ="";
+	DiaChi ="";
+	MSHS="";
+}
+HocSinh::HocSinh(string Ten, string Lop, string DiaChi, string NgaySinh, string  MSHS){
+	this->Ten=Ten;
+	this->DiaChi=DiaChi;
+	this->Lop=Lop;
+	this->NgaySinh=NgaySinh;
+	this->MSHS=MSHS;
+}
+HocSinh::~HocSinh(){
+	this->Ten="";
+	this->DiaChi="";
+	this->Lop="";
+	this->NgaySinh="";
+	this->MSHS="";
+}
+ void HocSinh::Nhap(){
 	fflush(stdin);
-	printf("\n Nhap ten nhan vien : ");
-	gets(x.name);
-	printf("\n Nhap tinh cua nhan vien : ");
-	gets(x.province);
-	printf("\n Nhap tuoi cua nhan vien : ");
-	scanf("%d",&x.age);
-	printf("\n Nhap ma so nhan vien : ");
-	scanf("%d",&x.msnv);
-	fflush(stdin);
-	printf("\n Nhap ten Truong :");
-	gets(x.truong);
-} 
-void NhapN(danhsach &ds){
-	printf("\n___________________________________________________________________\n");
-	for(int i = 0 ; i < ds.n ; i++ ){
-		printf("\n Thong tin nhan vien thu %d : ",i+1);
-	    Nhap(ds.a[i]);
-	}
-	printf("\n___________________________________________________________________\n");
+	cout<<"\n Nhap ten :";
+    getline(cin,Ten);
+    cout<<"\n Nhap lop :";
+    fflush(stdin);
+    getline(cin,Lop);
+    cout<<"\n Nhap dia chi :";
+    fflush(stdin);
+    getline(cin,DiaChi);
+    cout<<"\n Nhap ngay sinh (vd 23/02/2000):";
+    fflush(stdin);
+    getline(cin,NgaySinh);
+    cout<<"\n Nhap ma so hoc sinh : "<<endl;
+    cin>>MSHS;
+    Diem::Nhap1();
 }
-void Xuat(NV x){
-	fflush(stdin);
-     printf("\n ",x.name,x.province,x.age,x.msnv,x.truong);
-}
-void XuatN(danhsach ds){
-	printf("\n                  Xuat danh sach nhan vien                          \n");
-    printf("\n____________________________________________________________________\n");
-    printf("%-20s %-20s %-15s %15s  %15s \n", "Ho Ten "," Tinh"," Tuoi","Ma So Nhan Vien","Truong");
-    for(int i = 0 ; i< ds.n ; i++){
-    	Xuat(ds.a[i]);
-	}
-	printf("\n___________________________________________________________________\n");	
-}
-void ThemNV(danhsach &ds , NV &x, int b){
-	for(int i = ds.n ; i > b ;i--){
-		ds.a[i]=ds.a[i-1];
-	}
-	ds.a[b] = x;
-	ds.n++;
-}
-void TimNvTheoTinh(danhsach ds, char c[] ){
-	fflush(stdin);
-	printf("%-20s %-20s %-15s %-15s\n","Ho Ten ","Tinh","Tuoi","Ma So Nhan Vien");
-	for( int i = 0 ; i < ds.n ; i++){
-		if(strcmp(ds.a[i].name,c)==0){
-			Xuat(ds.a[i]);
-		}
-	}
-	
-}
-void Sapxep(danhsach ds ){
-	Employee t;
-	for(int i = 0; i < ds.n ; i++){
-		for(int j=i+1; j< ds.n; j++){
-			if(strcmp(ds.a[i].province , ds.a[j].province) < 0){
-				t=ds.a[i];
-				ds.a[i]=ds.a[j];
-				ds.a[j]=t;
-			}
-		}
-	}
-	XuatN(ds);
-}
-void ThongKe(danhsach ds ){
-	int dem = 0 ;
-	for( int i = 0 ; i < ds.n ; i++){
-		dem++;
-		if(strcmp(ds.a[i].province,ds.a[i+1].province)!=0){
-			printf("\n\t %s co %d nhan vien .",ds.a[i].province,dem);
-		     dem = 0;
-       	}
-	}
+void HocSinh::Xuat(){
+     cout<<"\tTen     \t\tLop     \t\tDia chi     \t\tMa hoc Sinh "<<endl;
+	 cout<<"\t"<<Ten<<"     \t\t"<<Lop<<"    \t\t"<<DiaChi<<"     \t\t"<<MSHS<<endl;
+	Diem::Xuat1();
 }
 
-void xuatFile(danhsach ds, char fileName[]){
-    FILE * fp;
-    fp = fopen (fileName,"w");
-    fprintf(fp, "%-20s %-20s %-20s %-20s\n", "Ho Ten","Tinh", "Tuoi", "Ma So Nhan Vien");
-    for(int i = 0;i < ds.n;i++){
-        fprintf(fp, "\n%-20s %-20s %-20d %-20d %\n", ds.a[i].name, ds.a[i].province, ds.a[i].age, ds.a[i].msnv);
+void input(HocSinh s[], int n){
+    for(int i=0; i<n; i++){
+        cout<<"thong tin hoc sinh  "<<i+1<<endl;
+         s[i].Nhap();
     }
-    fclose (fp);
 }
-void menu(){
-	NV x;
-	danhsach ds;
-	char fileName[] = "employee.txt";
-	int chon,b, flat = 1;
-	char c[20];
-	bool daNhap = false ;
-	do{
-		printf("\n Nhap so luong nhan vien : ");
-		scanf("%d",&ds.n);
-	}while(ds.n<=0);
-	while(flat){
-		system("cls");
-		printf("\n ******************************************************************\n");
-		printf("**                CHUONG TRINH QUAN LY NHAN VIEN                  **\n");
-		printf("**            01:    Nhap du lieu                                 **\n");
-		printf("**            02:    In danh sach nhan vien                       **\n");
-	    printf("**            03:    Them Nhan vien                               **\n");
-	    printf("**            04:    Tim kiem nhan vien theo tinh                 **\n");
-	    printf("**            05:    Sap xep nhan vien theo ten (Z->A)            **\n");
-	    printf("**            06:    Thong ke nhan vien theo tinh                 **\n");
-	    printf("**            07:    Ghi thong tin qua file                       **\n");
-	    printf("**            08:    Thoat                                        **\n");
-	    printf("*********************************************************************\n");
-	    printf("\nNhap lua chon cua ban : ");
-	    scanf("%d",&chon);
-	switch(chon){
-		case 1:{
-			printf("\n Ban da chon nhap ten nhan vien !");
-			NhapN(ds);	
-			printf("\n Ban da nhap thanh cong !\n");
-			daNhap=true;
-			printf("\n Vui long nhap phim bat ki de tiep tuc !");
-			getch();
-			break;
-		}
-		case 2: {
-		     if(daNhap){
-                    printf("\nBan da chon xuat DS  nhan vien!");
-                    XuatN(ds);
-              }
-			  else{
-                  printf("\nNhap DS nhan vien truoc!!!!");
-                }
-                printf("\nBam phim bat ky de tiep tuc!");
-                getch();
-                break;
+void print(HocSinh s[], int n){
+    for(int i=0; i<n; i++){
+        cout<<" ThongTin hoc sinh thu   "<<i+1<<endl;
+        s[i].Xuat();
+       
+    }
+}
+// sap xep theo ten
+void sapXepTen(HocSinh s[], int n){
+    HocSinh temp;
+    for (int i = 0; i < n-1; i++){
+        for(int j = i+1; j < n; j++){
+            if (strcmp(s[i].Ten,s[j].Ten) > 0){
+                temp = s[i];
+                s[i] = s[j];
+                s[j] = temp;
             }
-		case 3:{
-			if(daNhap){
-		
-			    do{
-			    printf("\n Them nhan vien\n ");
-				printf(" Nhap so vi tri can chen :");
-				scanf("%d",&b);
-				if(b >ds.n){
-					printf(" VUI LONG NHAP LAI !!!!");
-				}
-			}while(b>ds.n);
-	
-			    Nhap(x);
-				ThemNV(ds,x,b);
-				printf("\n thong tin sinh vien da them vao :\n");
-				XuatN(ds);
-			}
-			 else{
-                  printf("\nNhap DS nhan vien truoc!!!!");
-                }
-            printf("\nBam phim bat ky de tiep tuc!");
-                getch();
-                break;
+        }
+    }
+}
+// sap xep theo diem
+void sapXepDiemTB(HocSinh s[], int n){
+    HocSinh temp ;
+    for (int i = 0; i < n -1; i++){
+        for(int j = i+1; j < n; j++){
+            if (s[i].DiemTB() > s[i].DiemTB()){
+                temp = s[i];
+                s[i] = s[j];
+                s[j] = temp;
             }
-		case 4:
-			{
-				if(daNhap){
-				fflush(stdin);
-				printf("\n Nhap ten nhan vien can tim :");
-				gets(c);
-				printf("\n Du lieu nhan vien can tim la \n\n");
-				printf("\n_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_\n");
-				fflush(stdin);
-			    TimNvTheoTinh(ds,c);
-			}
-			 else{
-                  printf("\nNhap DS nhan vien truoc!!!!");
-                }
-                printf("\nBam phim bat ky de tiep tuc!");
-                getch();
+        }
+    }
+}
+// xep loai
+void xeploai (float a){
+    if (a >= 8.0){
+        cout <<"Gioi" << endl;
+    }
+    else if (a >=7.0 && a < 8.0){
+        cout <<"Kha"<< endl;
+    }
+    else if (a >= 5.0 && a < 7.0){
+        cout <<"Trung Binh"<< endl;
+    }
+    else {
+        cout <<"Yeu"<< endl;
+    }
+}
+/*void menu();
+        cout << "       CHUONG TRINH QUAN LY HOC SINH C/C++\n             ";
+        cout << "*************************MENU**************************\n";
+        cout << "**  1. Nhap Thong tin hoc sinh                       **\n";
+        cout << "**  2. Xuat thong tin hoc sinh.                      **\n";
+        cout << "**  3. Xoa sinh vien boi ID.                         **\n";
+        cout << "**  4. Tim kiem sinh vien theo ID.                   **\n";
+        cout << "**  5. Them 1 hoc sinh .                             **\n";  
+        cout << "**  6. Sap xep sinh vien theo diem trung binh .      **\n";
+        out <<  "**   7. Xep loai hoc sinh .                          **\n";  
+        cout << "**  8. Sap xep sinh vien theo ten.                   **\n";
+        cout << "**  9. Thong ke sinh vien theo tinh                  **\n";
+        cout << "**  10. Ghi danh sach hoc sinh vao file.             **\n";
+        cout << "**  0. Thoat                                         **\n";
+        cout << "*******************************************************\n";
+        cout << "Nhap tuy chon: ";
+        cin >> key;
+        switch(key){
+            case 1:
+                
+            case 2:
+                
+            case 3:
+                
+            case 4:
+               
+            case 5:
+                
+            case 6:
+                
+            case 7:
+                
+                   
+            case 8:
+                if(soluongSV > 0){
+               
+            case 0:
+               
+            default:
+                cout << "\nKhong co chuc nang nay!";
+                cout << "\nHay chon chuc nang trong hop menu.";
+                pressAnyKey();
                 break;
-            }
-	    case 5: {
-	    	if(daNhap){
-	    	printf(" Thong tin nhan vien sau khi sap xep la: \n");
-	       	Sapxep(ds);
-		    fflush(stdin);
-		}
-		 else{
-                  printf("\nNhap DS nhan vien truoc!!!!");
-                }
-                printf("\nBam phim bat ky de tiep tuc!");
-                getch();
-                break;
-            }
-		case 6:{
-			if(daNhap){
-			fflush(stdin);
-			printf("\n Thong ke nhan vien theo tinh \n");
-			ThongKe(ds);
-		}
-		 else{
-                  printf("\nNhap DS nhan vien truoc!!!!");
-                }
-                printf("\nBam phim bat ky de tiep tuc!");
-                getch();
-                break;
-            }
-		 
-		case 7:{
-			if(daNhap){
-                    printf("\nBan da chon xuat DS SV!");
-                    xuatFile(ds,fileName);
-                }
-			else{
-                   printf("\nNhap DS SV truoc!!!!");
-           }
-            printf("\nXuat DSSV thanh cong vao file %s!", fileName);
-            printf("\nBam phim bat ky de tiep tuc!");
-            getch();
-		  	break;
-		}
-		
-		 case 8:{
-		    printf(" Ban da  chon thoat chuong trinh !");
-		    flat = 0;
-		    break;
-	     }
-		default:
-                printf("\nKhong co chuc nang nay!");
-                printf("\nBam phim bat ky de tiep tuc!");
-                getch();
-                break;	         
-	  } 
-  }	    
+        }
+    }
+}
+*/
+int main(){
+	int n;
+	cout<<"Nhap  n: "; 
+    cin>>n;
+	HocSinh a[n+1];
+	//a.Nhap();
+	//a.Xuat();
+	input(a,n);
+    print(a,n);
+	//get(hs,b);
 }
